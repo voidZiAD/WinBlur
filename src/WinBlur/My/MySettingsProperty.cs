a0a07d78e402ef742a6ebd6224aaba16d3329f9b
Pronner <pronnerdev@gmail.com>
2022-12-31 21:20:27 +0400

    Add files via upload

---
+++ src/WinBlur/My/MySettingsProperty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace WinBlur.My
+{
+	// Token: 0x02000007 RID: 7
+	[StandardModule]
+	[HideModuleName]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal sealed class MySettingsProperty
+	{
+		// Token: 0x17000009 RID: 9
+		// (get) Token: 0x06000012 RID: 18 RVA: 0x000022E8 File Offset: 0x000004E8
+		[HelpKeyword("My.Settings")]
+		internal static MySettings Settings
+		{
+			get
+			{
+				return MySettings.Default;
+			}
+		}
+	}
+}
